45f1d07514120b6b29673d2e65ef305de6e71c57
aryadeepak817 <70505560+aryadeepak817@users.noreply.github.com>
2020-11-10 00:17:41 +0530

    Update FizzBuzzExceptionHandler.java

--- src/main/java/com/hackerrank/restcontrolleradvice/exception/FizzBuzzExceptionHandler.java
+++ src/main/java/com/hackerrank/restcontrolleradvice/exception/FizzBuzzExceptionHandler.java
@@ -4,7 +4,40 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class FizzBuzzExceptionHandler extends ResponseEntityExceptionHandler {
-
-  //TODO:: implement handler methods for FizzException, BuzzException and FizzBuzzException
+public class FizzBuzzExceptionHandler extends ResponseEntityExceptionHandler 
+{
+ //TODO:: implement handler methods for FizzException, BuzzException and FizzBuzzException
+ @ExceptionHandler(FizzException.class)
+ public ResponseEntity<Object> handleExceptions(FizzException exception, WebRequest webRequest)
+ {
+    ExceptionResponse response = new ExceptionResponse();
+    response.setDateTime(LocalDateTime.now());
+    response.setMessage("Not found");
+    
+    ResponseEntity<Object> entity = new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+        return entity;
+ }
+ 
+ @ExceptionHandler(BuzzException.class)
+ public ResponseEntity<Object> handleExceptions(BuzzException exception, WebRequest webRequest)
+ {
+    ExceptionResponse response = new ExceptionResponse();
+    response.setDateTime(LocalDateTime.now());
+    response.setMessage("Not found");
+    
+    ResponseEntity<Object> entity = new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+        return entity;
+ }
+ 
+ @ExceptionHandler(FizzBuzzException.class)
+ public ResponseEntity<Object> handleExceptions(FizzBuzzException exception, WebRequest webRequest)
+ {
+    ExceptionResponse response = new ExceptionResponse();
+    response.setDateTime(LocalDateTime.now());
+    response.setMessage("Not found");
+    
+    ResponseEntity<Object> entity = new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+        return entity;
+ }
+ 
 }
